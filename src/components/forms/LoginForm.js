441679cabe11fc0d50e52859164e4fbd8baaa4a3
Mugoya Dihfahsih <34502352+Dihfahsih1@users.noreply.github.com>
2019-07-22 15:36:58 +0300

    Login form with client-side validation.

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,66 +1,63 @@
 import React from "react";
 import { Button } from "reactstrap";
+import PropTypes from 'prop-types';
 import { AvForm, AvField } from "availity-reactstrap-validation";
-
+import Validator from 'validator';
+import InlineError from '../messages/InlineErrors';
 export default class LoginForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { email: false };
+  state = {
+    data:{
+    email:'',
+    password:''
+    },
+    loading:false,
+    errors:{}
+  };
+  onChange= e => this.setState(
+    {data:{...this.state.data,[e.target.name]: e.target.value}});
+  onSubmit = () =>{
+    const errors = this.validate(this.state.data);
+    this.setState({errors});
+    if(Object.keys(errors).length===0){
+      this.props.submit(this.state.data);
+    }
+  };
+  validate = (data) => {
+    const errors = {};
+    if(!Validator.isEmail(data.email)) errors.email = "invalid email";
+    if (!data.password) errors.password = "password can't be blank";
+    return errors;
   }
 
-  handleValidSubmit = (event, values) => {
-    this.setState({ email: values.email });
-    console.log(`Login Successful`);
-  };
-
-  handleInvalidSubmit = (event, errors, values) => {
-    this.setState({ email: values.email, error: true });
-    console.log(`Login failed`);
-  };
-
   render() {
+    const {data, errors} = this.state;
     return (
       <div className="card col-lg-6">
-      <AvForm
-        onValidSubmit={this.handleValidSubmit}
-        onInvalidSubmit={this.handleInvalidSubmit}
-      >
+      <AvForm onSubmit={this.onSubmit}>
         <AvField
+          error={!!errors.email}
           name="email"
           label="Email"
           type="text"
-          validate={{
-            required: true,
-            email: true
-          }}
+          value={data.email}
+          onChange={this.onChange}
         />
+        {errors.email && <InlineError text={errors.email} />}
         <AvField
+          error={!!errors.password}
           name="password"
           label="Password"
           type="password"
-          validate={{
-            required: {
-              value: true,
-              errorMessage: "Please enter your password"
-            },
-            pattern: {
-              value: "^[A-Za-z0-9]+$",
-              errorMessage:
-                "Your password must be composed only with letter and numbers"
-            },
-            minLength: {
-              value: 6,
-              errorMessage: "Your password must be between 6 and 16 characters"
-            },
-            maxLength: {
-              value: 16,
-              errorMessage: "Your password must be between 6 and 16 characters"
-            }
-          }}
+          value={data.email}
+          onChange={this.onChange}
         />
+        {errors.password && <InlineError text={errors.password} />}
         <Button id="submit">Submit</Button>
       </AvForm>
       </div>
     );
   }
+}
+LoginForm.propTypes = {
+  submit: PropTypes.func.isRequired
 }
